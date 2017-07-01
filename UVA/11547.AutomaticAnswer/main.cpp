78e3e10038b648ccc75fcca96c7fa049af137cf7
FabyGata <fabita.aliaga@gmail.com>
2017-07-01 11:31:05 -0400

    superizi

---
+++ UVA/11547.AutomaticAnswer/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int t, n;
+  cin>>t;
+  while(t--) {
+    cin>>n;
+    n*=567;
+    n/=9;
+    n+=7492;
+    n*=235;
+    n/=47;
+    n-=498;
+    n%=100;
+    n/=10;
+    cout<<abs(n)<<endl;
+  }
+  return 0;
+}
